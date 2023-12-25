506567bb2e37e6a676524db8b0f5456f751f2a76
sujaykadam <sujaykadam99@yahoo.com>
2023-12-25 11:08:36 -0700

    Fix SSR issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import type { AppProps } from 'next/app'
 import theme from '../chakra/theme'
 
 function MyApp({ Component, pageProps: {session, ...pageProps} }: AppProps) {return (
-	<SessionProvider session={pageProps.session}>
+	<SessionProvider session={session}>
 		<ChakraProvider theme={theme}>
 			<Component {...pageProps} />
 		</ChakraProvider>
