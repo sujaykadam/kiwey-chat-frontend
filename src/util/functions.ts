643e2f6743aabd5f0b545bc2af219d91312fde3f
sujaykadam <sujaykadam99@yahoo.com>
2023-12-28 19:27:08 -0700

    Implement formatUsernames util function

---
+++ src/util/functions.ts
@@ -0,0 +1,12 @@
+import { participantPopulated } from "../../../kiwey-chat-backend/src/util/types";
+
+export const formatUsernames = (
+	participants: Array<participantPopulated>,
+	myUserId: string
+): string => {
+	const usernames = participants
+		.filter((participant) => participant.user.id != myUserId)
+		.map((participant) => participant.user.username);
+
+	return usernames.join(", ");
+};
