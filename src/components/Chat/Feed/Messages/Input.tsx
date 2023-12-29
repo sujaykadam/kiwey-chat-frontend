ec2233188b8051c6461f7542820a6a96758b6580
sujaykadam <sujaykadam99@yahoo.com>
2023-12-28 21:10:33 -0700

    Create and show messageInput component

---
+++ src/components/Chat/Feed/Messages/Input.tsx
@@ -0,0 +1,46 @@
+import { Box, Input } from "@chakra-ui/react";
+import { Session } from "next-auth";
+import * as React from "react";
+import toast from "react-hot-toast";
+
+interface MessageInputProps {
+	session: Session;
+	conversationId: string;
+}
+
+const MessageInput: React.FunctionComponent<MessageInputProps> = ({
+	session,
+	conversationId,
+}: MessageInputProps) => {
+	const [messageBody, setMessageBody] = React.useState("");
+
+	const onSendMessage = async (event: React.FormEvent) => {
+		event.preventDefault();
+		try {
+			// call send message mutation
+		} catch (error: any) {
+			console.log("onSendMessageError", error);
+			toast.error(error.message || "An error occurred");
+		}
+	};
+	return (
+		<Box px={4} py={6} width="100%">
+			<form onSubmit={onSendMessage}>
+				<Input
+					value={messageBody}
+					size="md"
+					placeholder="New message"
+					_focus={{
+						boxShadow: "none",
+						border: "1px solid",
+						borderColor: "whiteAlpha.400",
+					}}
+					resize={"none"}
+					onChange={(event) => setMessageBody(event.target.value)}
+				/>
+			</form>
+		</Box>
+	);
+};
+
+export default MessageInput;
